6ad4dad8b1b07e361e71f2aa9aef5eb12c9bf191
Saoki11111 <t.aoki11111@gmail.com>
2023-10-04 21:27:01 +0900

    task の追加、削除の処理を実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-todo';
+  tasks = [
+    {title: '牛乳を買う', done: false},
+    {title: '可燃ごみを出す', done: true},
+    {title: '銀行に行く', done: false},
+  ];
+
+  newTaskTitle = '';
+
+  addTask() {
+    this.tasks.push({title: this.newTaskTitle, done: false});
+    this.newTaskTitle = '';
+  }
 }
